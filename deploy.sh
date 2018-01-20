3271f544b8b126a0bb4198cb499c381c1a9ca6b9
Robin Lovelace <rob00x@gmail.com>
2018-01-20 13:36:15 +0000

    Add all book files - simpler

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ update_website() {
   
   cp -fvr $BASE_REPO/_book/* .
   git add *.html; git add libs/; git add style.css;
-  git add _main_files/*; git add *.json; git add main.md
+  git add -A
   git commit -a -m "Updating book (${TRAVIS_BUILD_NUMBER})"
   git status
   git push 2>err.txt
